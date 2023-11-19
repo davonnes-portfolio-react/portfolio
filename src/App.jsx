3d4393643317910fd8ae83f0bf406dab76ce46ac
Vonney <davonnejv@gmail.com>
2023-11-19 14:21:22 -0600

    changing background color on NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { NavBar } from "./components/NavBar";
+import About from "./components/About";
 
 export default function App() {
   return (
-    <div className="bg-purple-600 p-5 w-full">
-      <NavBar />
-    </div>
+    <>
+      <div className="bg-pink-300 p-5 w-full">
+        <NavBar />
+      </div>
+
+      <About />
+    </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <>
+      <main className="w-full">
+        <h2 className="ml-10 mt-20 text-4xl font-serif">Davonne J. Vigil</h2>
+      </main>
+    </>
+  );
+}
