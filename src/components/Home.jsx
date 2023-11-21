3c1b153f43aad7956f509572324aa9565e3dd79f
Vonney <davonnejv@gmail.com>
2023-11-21 12:36:40 -0600

    adding typewriter-efffect on Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
@@ -9,11 +10,23 @@ export default function Home() {
   };
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-cover bg-[url('images/lights.jpg')]">
-      <span className="text-black text-6xl font-lobster ">
-        {" "}
-        I am a Front-End Developer
-      </span>
-
+      <span className="font-lobster text-7xl">I am a</span>
+      <div className="font-lobster text-6xl">
+        <Typewriter
+          options={{
+            strings: [
+              "Front-End Web Developer",
+              "Github Project Wizard",
+              "dedicated team player",
+            ],
+            autoStart: true,
+            delay: 150,
+            pauseFor: 2000,
+            deleteSpeed: 90,
+            loop: true,
+          }}
+        />
+      </div>
       <button
         type="button"
         onClick={goToMain}
